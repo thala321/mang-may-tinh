c92948afa59d0001bb21641ed379b05432ed3b4a
Sprise X <phanthanhhuy1996@gmail.com>
2020-05-02 14:29:48 +0700

    remove unused device code

--- device/raspberry.py
+++ device/raspberry.py
@@ -27,13 +27,8 @@ while True:
         camera.capture('/data/image.jpg')
         convertedImage = convertImageToBase64()
         try:
-            headers = {"Origin": "https://www.wes.org",
-               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-               "Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1", "Connection": "close",
-               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36",
-               "Referer": "https://www.wes.org/appstatus/indexca.aspx", "Accept-Encoding": "gzip, deflate, br",
-               "Accept-Language": "en-US,en;q=0.8,fa;q=0.6", "Content-Type": "application/json"}
-            r = requests.post("https://192.168.43.223:5001/api/vehicles", json={"cardId": "foRAAIBAgQEAwQHBQQEAAECdwABAgMRBAUhMQYSQVEHYXETIj", "vehiclePlatesImg": convertedImage, "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"}, headers=headers, verify=False)
+            headers = {"Content-Type": "application/json"}
+            r = requests.post("https://192.168.43.223:5001/api/vehicles", json={"cardId": rfidData, "vehiclePlatesImg": convertedImage, "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"}, headers=headers, verify=False)
             r.raise_for_status()
         except requests.exceptions.HTTPError as e:
             print (e.response.text)
